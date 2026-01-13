1df809aeb34881c45c2a782cc4260b36a5ebfe9d
wonseonghun41 <wonseonghun41@gmail.com>
2026-01-13 16:16:19 +0900

    01.13 공부 :)

---
+++ Test.py
@@ -0,0 +1,12 @@
+#----------------------------------------17강 조건문 기초 문제풀이
+age = 40
+if age >= 10 and age < 20:
+  print("10대")
+elif age >= 20 and age < 30:
+  print("20대")
+elif age >= 30 and age < 40:
+  print("30대")
+elif age >= 40 and age < 50:
+  print("40대")
+else :
+  print("50대 이상")
